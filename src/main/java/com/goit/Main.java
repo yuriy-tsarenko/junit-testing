da5366ed41c38055ea954a8ee1806e08a08acfe4
Yuriy Tsarenko <tsarenkoyuriidev@gmail.com>
2023-04-03 20:01:35 +0300

    init

---
+++ src/main/java/com/goit/Main.java
@@ -0,0 +1,7 @@
+package com.goit;
+//https://gorest.co.in/public/v2/users
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/goit/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.goit.dto;
+//{
+//"id": 695102,
+//"name": "Fr. Dakshayani Khan",
+//"email": "fr_khan_dakshayani@ferry.name",
+//"gender": "female",
+//"status": "inactive"
+//}
+public class UserDto {
+}
